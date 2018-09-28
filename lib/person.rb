28ec6c5fa61b654d3d276e9c22742ecebf4cfd9e
Peter Stone <peter.stone.online@gmail.com>
2018-09-28 17:02:52 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+ 
+  def name=(persName)
+    @name = persName
+  end
+ 
+  def name
+    @name
+  end
+  
+  def job=(jobName)
+    @job = jobName 
+  end 
+  
+  def job 
+    @job 
+  end 
+end
